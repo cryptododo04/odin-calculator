c85c38069aaa5815309350bee0747ac22523ef43
santi cuda <santiagoacuda@gmail.com>
2022-12-29 18:50:48 -0300

    added decimal button function

--- main.js
+++ main.js
@@ -41,18 +41,39 @@ document.addEventListener("DOMContentLoaded",function(){
         currentScreen.textContent = currentValue;
     })
 
+    equal.addEventListener("click",function(){
+
+        if(currentValue != "" && previousValue != ""){
+        
+        calculate();
+        previousScreen.textContent = "";
+        if(previousValue.length <= 7){
+            currentScreen.textContent = previousValue;
+        }
+        else
+        {
+            currentScreen.textContent = previousValue.slice(0,7) + "...";
+        }
+        }
+    })
+
+    decimal.addEventListener("click", function(){
+        addDecimal();
+        currentScreen.textContent = currentValue;
+    })
+
 })
 
 
-
+//handle clicked number button
 function handleNumber(num){
-    if(currentValue.length <= 5)
+    if(currentValue.length <= 7)
     {
     currentValue += num;
     }
 }
 
-
+//handle clicked operator button
 function handleOperator(op){
 
     operator = op;
@@ -60,3 +81,40 @@ function handleOperator(op){
     currentValue = "";
 }
 
+//calculate after equal button clicked
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue; 
+    }
+    else if(operator === "-"){
+        previousValue -= currentValue;
+    }
+    else if(operator === "*"){
+        previousValue *= currentValue;
+    }
+    else{
+        previousValue /= currentValue;
+    }
+    
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+    console.log(previousValue);
+}
+
+//round numbers
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+
+//handle decimal number clicked
+function addDecimal(){
+    if(!currentValue.includes('.')){
+        currentValue += '.';
+    }
+
+}
